992d0a094a111a33961854b15e5b056b86971c35
William Goode <wgoode3@gmail.com>
2019-04-06 09:55:27 -0400

    added docs

---
+++ docs/precache-manifest.240b2fcf6bfee9e83ad7c6fa881b943a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f76aa706a265f3dd6ccf",
+    "url": "/react-dojodatchi/static/js/runtime~main.817fe3ed.js"
+  },
+  {
+    "revision": "fa17ad49f82feac5afef",
+    "url": "/react-dojodatchi/static/js/main.cc29bbbc.chunk.js"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/react-dojodatchi/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "fa17ad49f82feac5afef",
+    "url": "/react-dojodatchi/static/css/main.1d88ab6c.chunk.css"
+  },
+  {
+    "revision": "d0004a5fc2e19f4593e74faed6ab2132",
+    "url": "/react-dojodatchi/index.html"
+  }
+];
